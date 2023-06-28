73ae3299b6d8ce300d7855cf351463c46b8047b6
Ab <abishekab3344@gmail.com>
2023-06-28 16:02:22 +0530

    added media query

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import {HashRouter,Routes,Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <HashRouter>
     <Navbar />
     <Routes>
@@ -27,11 +27,4 @@ root.render(
       <Route path="*" element={<Nopage />} />
     </Routes>
     </HashRouter>
-  
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
